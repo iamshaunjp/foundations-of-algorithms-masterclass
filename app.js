210e01b34a71b12d7fbbb2fc20bc1555338ac84d
iamshaunjp <shaun@thenetninja.co.uk>
2024-11-27 12:16:00 +0000

    lesson-23

--- app.js
+++ app.js
@@ -2,18 +2,15 @@ function fib(n) {
   // base cases
   if (n < 1) return 0
   if (n === 1) return 1
-
-  let prev = 1 // 1st number in fib sequence
-  let current = 1 // 2nd number in fib sequence
-
-  for (let i = 3; i <= n; i++) {
-    let next = current + prev
-    prev = current
-    current = next
-  }
-
-  return current
+  
+  return fib(n - 1) + fib(n - 2)
 }
 
-// fibonacci sequence
-// 1, 1, 2, 3, 5, 8, 13, 21
+/*
+
+Example with n=4
+  --> return fib(3) + fib(2) 
+    --> return fib(2) + fib(1)->1 + fib(1)->1 + fib(0)->0
+      --> return fib(1)->1 + fib(0)->0
+
+*/
