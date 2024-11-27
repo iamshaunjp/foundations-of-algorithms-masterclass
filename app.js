c537c38f849b4ed72989e77804ebc6d61bb8aa30
iamshaunjp <shaun@thenetninja.co.uk>
2024-11-27 11:11:21 +0000

    lesson-7

--- app.js
+++ app.js
@@ -1,35 +1,5 @@
-// version 1 - loop
-function sumToNLoop(n) {
-  let total = 0
-  for (let i = 1; i <= n; i++) {
-      total += i
-  }
-  return total
-}
-
-// version 2 - formula
 function sumToNFormula(n) {
   return (n * (n + 1)) / 2
 }
 
-
-
-// let start = performance.now()
-// sumToNLoop(10)
-// let end = performance.now()
-// console.log(`time taken: ${(end - start)} ms`)
-
-// start = performance.now()
-// sumToNLoop(100000000) // 1 hundred mill
-// end = performance.now()
-// console.log(`time taken: ${(end - start)} ms`)
-
-// start = performance.now()
-// sumToNFormula(10) 
-// end = performance.now()
-// console.log(`time taken: ${(end - start)} ms`)
-
-// start = performance.now()
-// sumToNFormula(100000000) // 1 hundred mill
-// end = performance.now()
-// console.log(`time taken: ${(end - start)} ms`)
+// Time Complexity - O(n)
