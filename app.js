18f044429c3847ef1e680ecc1fb05a0db5a9279f
iamshaunjp <shaun@thenetninja.co.uk>
2024-11-27 13:27:47 +0000

    lesson-27

--- app.js
+++ app.js
@@ -1,32 +1,6 @@
-function fib(n, memo = {}) {
-  // check if the value is already in the memo
-  if (n in memo) return memo[n]
-
-  // base cases
-  if (n < 1) return 0
+function countStaircaseWays(n) {
   if (n === 1) return 1
+  if (n === 2) return 2
 
-  // recursive calls with memoization
-  memo[n] = fib(n - 1, memo) + fib(n - 2, memo)
-
-  return memo[n]
+  return countStaircaseWays(n - 1) + countStaircaseWays(n - 2)
 }
-
-// Time complexity --> O(n)
-
-
-/*
-
-INPUT: n=5
---> memo[5] = fib(4) + fib(3)
-  --> memo[4] = fib(3) + fib(2)
-    --> memo[3] = fib(2) + fib(1)
-      --> memo[2] = fib(1) + fib(0) = 1
-
-{
-  2: 1,
-  3: 2,
-  4: 3
-}
-
-*/
