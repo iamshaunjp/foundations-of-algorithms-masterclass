cbf788327967050d9727ec06d2a7d650466109b3
iamshaunjp <shaun@thenetninja.co.uk>
2024-11-27 11:45:33 +0000

    lesson-19

--- app.js
+++ app.js
@@ -1,36 +1,15 @@
-// Example 1 - constant space O(1)
-function addNumbers(a, b) {
-  return a + b
-}
-
-// Example 2 - linear space O(n)
-function createNumberList(n) {
-  let numberList = []
-
-  for (let i = 0; i < n; i++) {
-    numberList.push(i)
+function countdown(num) {
+  if (num <= 0) { // this is the base case
+    console.log('done!')
+    return // no more recursions
   }
 
-  return numberList
+  console.log(num)
+  countdown(num - 1)
 }
 
-// Example 3 - quadratic space O(n^2)
-function createPairs(n) {
-  let pairs = []
 
-  for (let i = 0; i < n; i++) {
-    let row = []
 
-    for (let j = 0; j < n; j++) {
-      row.push([i, j])
-    }
-
-    pairs.push(row)
-  }
-
-  return pairs
+function infiniteRecursion() {
+  infiniteRecursion()
 }
-
-// for n = 3:
-// [[[0,0],[0,1],[0,2]], [[1,0],[1,1],[1,2]], [[2,0],[2,1],[2,2]]]
-// --> 3 x 3 = 9 pairs in total
