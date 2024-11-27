0cd2bd184e0c712a122e5bb4a6be892e70260977
iamshaunjp <shaun@thenetninja.co.uk>
2024-11-27 11:32:30 +0000

    lesson-14

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 // given a mixed array of positive and negative integers, return the total number of only positive integers
 
 function countPositiveNumbers(arr) {
+  let count = 0
 
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > 0) {
+      count++
+    }
+  }
 
-
+  return count
 }
- 
+
+// create variable to count pos numbers
+
+// loop through array and look at each number
+  // check if number is > 0
+    // if so, add 1 to the count
