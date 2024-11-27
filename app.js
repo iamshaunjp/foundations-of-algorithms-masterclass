5da0679de3e4dd7243cf5fb902d88308416b751e
iamshaunjp <shaun@thenetninja.co.uk>
2024-11-27 17:21:45 +0000

    lesson-32

--- app.js
+++ app.js
@@ -1,25 +1,7 @@
-function fib(n, memo = {}) {
-  // check if the value is already in the memo
-  if (n in memo) return memo[n]
+function linearSearch(arr, num){
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === num) return i
+  }
 
-  // base cases
-  if (n < 1) return 0
-  if (n === 1) return 1
-
-  // recursive calls with memoization
-  memo[n] = fib(n - 1, memo) + fib(n - 2, memo)
-
-  return memo[n]
+  return -1
 }
-
-// Time complexity --> O(n)
-// Space complexity --> O(n)
-
-/*
-
-INPUT: n=5
---> memo[5] = fib(4) + fib(3)
-  --> memo[4] = fib(3) + fib(2)
-    --> memo[3] = fib(2) + fib(1)
-      --> memo[2] = fib(1) + fib(0) = 1
-*/
