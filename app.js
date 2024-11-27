6edf44598b12d7cff99269196525bef9f675d09e
iamshaunjp <shaun@thenetninja.co.uk>
2024-11-27 12:12:23 +0000

    lesson-22

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
-function countStaircaseWays(n) {
-  // bases cases
+function fib(n) {
+  // base cases
+  if (n < 1) return 0
   if (n === 1) return 1
-  if (n === 2) return 2
 
-  // recursiv calls for (n - 1) and (n - 2)
-  return countStaircaseWays(n - 1) + countStaircaseWays(n - 2)
+  let prev = 1 // 1st number in fib sequence
+  let current = 1 // 2nd number in fib sequence
+
+  for (let i = 3; i <= n; i++) {
+    let next = current + prev
+    prev = current
+    current = next
+  }
+
+  return current
 }
 
-
-/* 
-
-Example --> n = 5
-  --> return count(4) + count(3)
-    --> return count(3) + count(2)->2 + count(2)->2 + count(1)->1
-      --> return count(2)->2 + count(1)->1
-
-*/
+// fibonacci sequence
+// 1, 1, 2, 3, 5, 8, 13, 21
