f9193756a276a0ac07e53ed5099fc221ef853625
iamshaunjp <shaun@thenetninja.co.uk>
2024-11-27 11:12:38 +0000

    lesson-8

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
-function sumToNFormula(n) {
-  return (n * (n + 1)) / 2
+// version 1 - loop
+function sumToNLoop(n) {
+  let total = 0
+
+  for (let i = 1; i <= n; i++) {
+      total += i
+  }
+  
+  return total
 }
 
 // Time Complexity - O(n)
