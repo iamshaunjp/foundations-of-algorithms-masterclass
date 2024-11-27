93b579bdb4800adea18ac8cd4dbabd81eeca8593
iamshaunjp <shaun@thenetninja.co.uk>
2024-11-27 13:24:22 +0000

    lesson-24

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
+// Staircase problem
+function countStaircaseWays(n) {
+  if (n === 1) return 1
+  if (n === 2) return 2
+
+  return countStaircaseWays(n - 1) + countStaircaseWays(n - 2)
+}
+
+// Fibonacci sequence
 function fib(n) {
-  // base cases
   if (n < 1) return 0
   if (n === 1) return 1
   
   return fib(n - 1) + fib(n - 2)
 }
-
-/*
-
-Example with n=4
-  --> return fib(3) + fib(2) 
-    --> return fib(2) + fib(1)->1 + fib(1)->1 + fib(0)->0
-      --> return fib(1)->1 + fib(0)->0
-
-*/
