e012b5ce811a588c5c6300539dc2a0dcb54a9604
iamshaunjp <shaun@thenetninja.co.uk>
2024-11-27 13:29:16 +0000

    lesson-28

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
-function countStaircaseWays(n) {
+function countStaircaseWays(n, memo = {}) {
+  // check if the value is already in the memo
+  if (n in memo) return memo[n]
+
+  // base cases
   if (n === 1) return 1
   if (n === 2) return 2
 
-  return countStaircaseWays(n - 1) + countStaircaseWays(n - 2)
+  memo[n] = countStaircaseWays(n - 1) + countStaircaseWays(n - 2)
+
+  return memo[n]
 }
