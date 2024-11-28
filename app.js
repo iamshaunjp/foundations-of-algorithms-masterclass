825388b503095be1b88ed27aa468afc0df6fa842
iamshaunjp <shaun@thenetninja.co.uk>
2024-11-28 15:31:08 +0000

    lesson-36

--- app.js
+++ app.js
@@ -1,22 +1,20 @@
-// CHALLENGE - turn this iterative algorithm into a recursive algorithm
-
-function binarySearch(arr, value) {
-  let left = 0
-  let right = arr.length - 1
-
-  while (left <= right) {
-    let middle = Math.floor((right + left)/2)
-
-    if (arr[middle] === value) return middle
-
-    if (arr[middle] < value) {
-      left = middle + 1
-    }
-    if (arr[middle] > value) {
-      right = middle - 1
-    }
+function binarySearch(arr, value, left = 0, right = arr.length - 1) {
+  // case for value not in the array
+  if (left > right) {
+    return -1
   }
 
-  return -1
-}
+  let middle = Math.floor((right + left) / 2)
 
+  if (arr[middle] === value) {
+    return middle
+  }
+
+  if (arr[middle] < value) {
+    return binarySearch(arr, value, middle + 1, right)
+  }
+
+  if (arr[middle] > value) {
+    return binarySearch(arr, value, left, middle - 1)
+  }
+}
