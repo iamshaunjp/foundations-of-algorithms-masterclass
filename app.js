62f578fe26ac29e239c570fed37718d6db1c51f4
iamshaunjp <shaun@thenetninja.co.uk>
2024-11-28 14:57:50 +0000

    lesson-34

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
-function linearSearch(arr, num){
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === num) return i
+function binarySearch(arr, value) {
+  let left = 0
+  let right = arr.length - 1
+
+  while (left <= right) {
+    let middle = Math.floor((right + left)/2)
+
+    if (arr[middle] === value) return middle
+
+    if (arr[middle] < value) {
+      left = middle + 1
+    }
+    if (arr[middle] > value) {
+      right = middle - 1
+    }
   }
 
   return -1
 }
+
+// Example input: [1,2,4,5,6,7,9,10,11,12]
+//  --> length = 12, target = 4
