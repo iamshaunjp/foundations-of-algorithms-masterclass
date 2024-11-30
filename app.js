bf304b06d19a8443fa3d064f4dee1f7bcc4ffbd0
iamshaunjp <shaun@thenetninja.co.uk>
2024-11-30 07:06:12 +0000

    lesson-40

--- app.js
+++ app.js
@@ -2,13 +2,16 @@
 
 function bubbleSort(arr) {
   for (i = arr.length - 1; i > 0; i--) {
+    console.log('new pass')
 
     for (j = 0; j < i; j++) {
       if (arr[j] > arr[j+1]) {
-        [arr[j], arr[j+1]] = [arr[j+1], arr[j]] 
+        [arr[j], arr[j+1]] = [arr[j+1], arr[j]]
+        swap = true 
       }
     }
 
+    console.log('array after pass:', arr)
   }
 
   return arr
