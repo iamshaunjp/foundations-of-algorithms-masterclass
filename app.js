c359217a720708f19417f783f74a3bd76e1f2ff2
iamshaunjp <shaun@thenetninja.co.uk>
2024-11-30 17:35:20 +0000

    lesson-44

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 function selectionSort(arr) {
-  // loop over the whole array n times, starting at 0
-    // treat i as the index of the starting "min" value for each pass
 
-    // loop the remaining unsorted array (from i to n) for each pass
-      // find the index of the smallest value in the remaining portion
-    
-    // swap that value with the value at the "min" position
+  for (let i = 0; i < arr.length - 1; i++) {
+    let minIndex = i
 
-    // repeat until the entire array is sorted
+    for (j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j
+      }
+    }
+
+    if (min !== i) {
+      [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]]
+    }
+  }
+
+  return arr
 }
