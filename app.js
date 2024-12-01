1595707c01f3868778a0737577243bf12e25baff
iamshaunjp <shaun@thenetninja.co.uk>
2024-12-01 11:01:53 +0000

    lesson-46

--- app.js
+++ app.js
@@ -1,18 +1,35 @@
-function selectionSort(arr) {
+// a helper function to merge two sorted arrays
+function mergeTwoArrays(arrayOne, arrayTwo) {
+  let totalLength = arrayOne.length + arrayTwo.length
+  let merged = []
+  let i = 0
+  let j = 0
 
-  for (let i = 0; i < arr.length - 1; i++) {
-    let minIndex = i
-
-    for (j = i + 1; j < arr.length; j++) {
-      if (arr[j] < arr[minIndex]) {
-        minIndex = j
-      }
+  while (merged.length < totalLength) {
+    // check if end of arrOne is reached
+    if (i >= arrayOne.length) {
+      merged.push(arrayTwo[j])
+      j++
+      continue
     }
 
-    if (min !== i) {
-      [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]]
+    // check if end of arrTwo is reached
+    if (j >= arrayTwo.length) {
+      merged.push(arrayOne[i])
+      i++
+      continue
     }
+
+    // check which is bigger and merge
+    if (arrayOne[i] <= arrayTwo[j]) {
+      merged.push(arrayOne[i])
+      i++
+      continue
+    }
+
+    merged.push(arrayTwo[j])
+    j++
   }
 
-  return arr
+  return merged
 }
