fad58316d1001da022863910ce2672129571b822
iamshaunjp <shaun@thenetninja.co.uk>
2024-12-02 17:35:12 +0000

    lesson-51

--- app.js
+++ app.js
@@ -1,30 +1,33 @@
-function countPaths(m, n, memo = {}) {
-  // base case - only one path if m or n is 1
-  if (m === 1 || n === 1) return 1
-
-  // base case - no path if m or n is 0
-  if (m < 1 || n < 1) return 0
-
-  // create a key for current grid position
-  const key = `${m},${n}`
-
-  // return memoized result if prev solved for this position
-  if (memo[key]) {
-    return memo[key]
+function areAnagrams(str1, str2) {
+  if (str1.length !== str2.length) {
+    return false
   }
 
-  memo[key] = countPaths(m - 1, n, memo) + countPaths(m, n - 1, memo)
+  // frequence object
+  const frequency = {}
 
-  return memo[key]
+  for (const char of str1) {
+    frequency[char] = (frequency[char] || 0) + 1;
+  }
+
+  for (const char of str2) {
+    if (!frequency[char]) {
+      return false
+    }
+    frequency[char]--
+  }
+
+  return true
 }
 
-/*
+/* str1: asleep, str2: please
 
-input: (3,3)
-
-  --> count(2,3) + count(3,2)
-  --> count(1,3) + count(2,2) + count(2,2) + count(3,1)
-  --> 1 + count(1,2) + count(2,1) + count(1,2) + count(2,1) + 1
-  --> 1 + 1 + 1 + 1 + 1 + 1 = 6
+{
+  a: 1,
+  s: 1,
+  l: 1,
+  e: 2,
+  p: 1
+}
 
 */
