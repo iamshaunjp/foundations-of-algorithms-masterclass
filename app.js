4233bd9ee9b552682fce53a55919764daab3275f
iamshaunjp <shaun@thenetninja.co.uk>
2024-12-02 13:49:28 +0000

    lesson-49

--- app.js
+++ app.js
@@ -1,62 +1,12 @@
-// a helper function to merge two sorted arrays
-function mergeTwoArrays(arrayOne, arrayTwo) {
-  let totalLength = arrayOne.length + arrayTwo.length
-  let merged = []
-  let i = 0
-  let j = 0
+function findLargestNumber(arr) {
+  if (arr.length === 1) return arr[0]
 
-  while (merged.length < totalLength) {
-    // check if end of arrOne is reached
-    if (i >= arrayOne.length) {
-      merged.push(arrayTwo[j])
-      j++
-      continue
-    }
+  const mid = Math.floor(arr.length / 2)
+  const left = arr.slice(0, mid)
+  const right = arr.slice(mid)
 
-    // check if end of arrTwo is reached
-    if (j >= arrayTwo.length) {
-      merged.push(arrayOne[i])
-      i++
-      continue
-    }
+  const leftMax = findLargestNumber(left)
+  const rightMax = findLargestNumber(right)
 
-    // check which is bigger and merge
-    if (arrayOne[i] <= arrayTwo[j]) {
-      merged.push(arrayOne[i])
-      i++
-      continue
-    }
-
-    merged.push(arrayTwo[j])
-    j++
-  }
-
-  return merged
+  return Math.max(leftMax, rightMax)
 }
-
-function mergeSort(arr) {
-  if (arr.length <= 1) {
-    return arr
-  }
-
-  let middle = Math.floor(arr.length/2)
-
-  let left =  mergeSort(arr.slice(0, middle))
-  let right = mergeSort(arr.slice(middle))
-
-  return mergeTwoArrays(left, right)
-}
-
-// input: [9,2,5,4]
-// --> left = mergeSort([9,2]) & right = mergeSort([5,4])
-// --> return mergeTwoArrays([2,9], [4,5]) = [2,4,5,9]
-
-// input: [9,2]            
-// --> left = mergeSort([9]) & right = mergeSort([2])
-// --> left = [9] & right = [2]
-// --> return mergeTwoArrays([9], [2]) = [2,9]
-
-// input: [5,4]
-// --> left = mergeSort([5]) & right = mergeSort([4])
-// --> left = [5] & right = [4]
-// --> return mergeTwoArrays([5], [4]) = [4,5]   
