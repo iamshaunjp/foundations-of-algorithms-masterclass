032333182f9c27c165f37ace245ec84dd8817376
iamshaunjp <shaun@thenetninja.co.uk>
2024-12-02 16:18:36 +0000

    lesson-50

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
-function findLargestNumber(arr) {
-  if (arr.length === 1) return arr[0]
+function countPaths(m, n, memo = {}) {
+  // base case - only one path if m or n is 1
+  if (m === 1 || n === 1) return 1
 
-  const mid = Math.floor(arr.length / 2)
-  const left = arr.slice(0, mid)
-  const right = arr.slice(mid)
+  // base case - no path if m or n is 0
+  if (m < 1 || n < 1) return 0
 
-  const leftMax = findLargestNumber(left)
-  const rightMax = findLargestNumber(right)
+  // create a key for current grid position
+  const key = `${m},${n}`
 
-  return Math.max(leftMax, rightMax)
+  // return memoized result if prev solved for this position
+  if (memo[key]) {
+    return memo[key]
+  }
+
+  memo[key] = countPaths(m - 1, n, memo) + countPaths(m, n - 1, memo)
+
+  return memo[key]
 }
+
+/*
+
+input: (3,3)
+
+  --> count(2,3) + count(3,2)
+  --> count(1,3) + count(2,2) + count(2,2) + count(3,1)
+  --> 1 + count(1,2) + count(2,1) + count(1,2) + count(2,1) + 1
+  --> 1 + 1 + 1 + 1 + 1 + 1 = 6
+
+*/
